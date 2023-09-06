20f12ba018192a56294c4de21653ee8bebd5d341
Linfern <122474303+linfern-mou@users.noreply.github.com>
2023-09-06 22:28:15 +0800

    Add files via upload

--- tvbox/json/低端影视.js
+++ tvbox/json/低端影视.js
@@ -96,11 +96,12 @@ if (/getvddr/.test(purl)) {
 }
 `;
 
-// 网址发布页 https://ddys.site
+// 网址发布页 https://ddys.site///https://ddys.info/
 var rule={
     title:'ddys',
-    host:'https://ddys.site', 
-    hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});HOST = jsp.pdfh(html,"a:eq(1)&&href")',
+    host:'https://ddys.pro',
+    //host:'https://ddys.site', 
+   // hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});HOST = jsp.pdfh(html,"a:eq(1)&&href")',
     url:'/fyclass/page/fypage/',
     searchUrl:'/?s=**&post_type=post',
     searchable:2,
